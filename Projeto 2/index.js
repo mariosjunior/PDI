2dc10b5cc71a6e8c0079fa59360b4923630874ea
mariosjunior <maas.jr@hotmail.com>
2021-12-14 17:59:57 -0300

    Continuação do setup do projeto

--- Projeto 2/index.js
+++ Projeto 2/index.js
@@ -1,13 +1,57 @@
 import express from 'express'
-import exphbs from 'express-handlebars'
-import sessions from 'sessions'
-import * as FileStore from 'session-file-store'
+import {
+    engine
+} from 'express-handlebars';
+import session from 'express-session'
+import sessionFileStore from 'session-file-store'
 import flash from 'express-flash'
 import {
     conn
 } from './db/conn.js'
-//const conn = require('./db/conn')
 const app = express()
+
+const FileStore = sessionFileStore(session);
+
+
+app.engine('handlebars', engine())
+app.set('view engine', 'handlebars')
+
+app.use(express.urlencoded({
+    extended: true
+}))
+
+app.use(express.json())
+
+app.use(session({
+    name: 'session',
+    secret: 'nosso_secret',
+    resave: false,
+    saveUninitialized: true,
+    store: new FileStore,
+    cookie: {
+        secure: false,
+        maxAge: 360000,
+        expires: new Date(Date.now() + 360000),
+        httpOnly: true
+    }
+}))
+
+//
+
+app.use(flash())
+
+//
+
+app.use(express.static('public'))
+
+//
+app.use((req, res, next) => {
+    if (req.session.userid) {
+        res.locals.session = req.session
+    }
+    next()
+})
+
 
 conn.sync().then(() => {
     app.listen(300)
