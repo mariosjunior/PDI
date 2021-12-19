b0bc77454211373f21ae8dbf962d2d1fa98698eb
mariosjunior <maas.jr@hotmail.com>
2021-12-19 20:01:13 -0300

    Atualizar pensamentos

--- Projeto 2/controllers/ToughtsController.js
+++ Projeto 2/controllers/ToughtsController.js
@@ -25,8 +25,17 @@ export default class ToughtController {
         }
 
         const toughts = user.Toughts.map((result) => result.dataValues)
+
+
+        let emptyToughts = false;
+
+        if (toughts.length === 0) {
+            emptyToughts = true;
+        }
+
         res.render('toughts/dashboard', {
-            toughts
+            toughts,
+            emptyToughts
         })
     }
 
@@ -64,6 +73,44 @@ export default class ToughtController {
                 }
             })
             req.flash('message', 'Pensamento removido com sucesso!')
+            req.session.save(() => {
+                res.redirect('/toughts/dashboard')
+            })
+        } catch (e) {
+            console.error(e)
+        }
+    }
+
+    static async updateTought(req, res) {
+        const id = req.params.id
+        const tought = await Tought.findOne({
+            where: {
+                id: id
+            },
+            raw: true
+        })
+
+        res.render('toughts/edit', {
+            tought
+        })
+    }
+
+    static async updateToughtSave(req, res) {
+
+        const id = req.body.id
+        const tought = {
+            title: req.body.title,
+
+        }
+
+        try {
+            await Tought.update(tought, {
+                where: {
+                    id: id
+                }
+            })
+
+            req.flash('message', 'Pensamento atualizado com sucesso!')
             req.session.save(() => {
                 res.redirect('/toughts/dashboard')
             })
--- Projeto 2/routes/toughtsRoutes.js
+++ Projeto 2/routes/toughtsRoutes.js
@@ -8,6 +8,8 @@ import {
 export const toughtsRoutes = express.Router()
 toughtsRoutes.get('/', ToughtController.showToughts)
 toughtsRoutes.get('/add', checkAuth, ToughtController.createTought)
+toughtsRoutes.get('/edit/:id', checkAuth, ToughtController.updateTought)
+toughtsRoutes.post('/edit', checkAuth, ToughtController.updateToughtSave)
 toughtsRoutes.post('/add', checkAuth, ToughtController.createToughtSave)
 toughtsRoutes.post('/remove', checkAuth, ToughtController.removeTought)
 toughtsRoutes.get('/dashboard', checkAuth, ToughtController.dashboard)
